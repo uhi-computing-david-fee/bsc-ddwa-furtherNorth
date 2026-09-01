13339928a5c761e8ebea2f32e9f1e11c7fe49a56
David Fee <davidfee36@gmail.com>
2026-09-01 15:35:59 +0100

    Finish nav

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,40 @@
 const menuToggle = document.querySelector('.menu-toggle');
+const menuLabel = document.querySelector('.menu-label');
+const menuLinks = document.querySelectorAll('.primary-nav a');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+
 
 menuToggle.addEventListener('click', () => {
     const isOpen = menuToggle.getAttribute('aria-expanded') === "true";
-    menuToggle.setAttribute("aria-expanded", String(!isOpen));
+    isOpen ? closeMenu() : openMenu();
+});
+
+function openMenu() {
+    menuToggle.setAttribute("aria-expanded", true);
+    menuLabel.textContent = "Close";
+    document.body.classList.add("menu-open"); // use to prevent background scrolling
+    main.inert = true; // take elements out of keyboard and interaction flow
+    footer.inert = true;
+}
+
+function closeMenu() {
+    menuToggle.setAttribute("aria-expanded", false);
+    menuLabel.textContent = "Menu";
+    document.body.classList.remove("menu-open"); 
+    main.inert = false; 
+    footer.inert = false;
+}
+
+// close menu with ESC key
+document.addEventListener('keydown', (e) => {
+    const isOpen = menuToggle.getAttribute('aria-expanded') === "true";
+    if (e.key === 'Escape' && isOpen) {
+        closeMenu();
+    }
+});
+
+// close menu when any link is clicked
+menuLinks.forEach(link => {
+    link.addEventListener('click', closeMenu);
 });
